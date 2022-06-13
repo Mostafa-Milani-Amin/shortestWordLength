28fdd8f90434146920a7326b46dca4bb97b6c8ce
Mostafa Milani Amin <90893927+Mostafa-Milani-Amin@users.noreply.github.com>
2022-06-13 18:05:46 +0430

    Add ".replace(/[\u2000-\u206F..."
    Ho aggiunto ".replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"()*+,\-.\/:;<=>?\[\]_`{|}]/g,"")" per escludere i segni di interpunzione dal calcolo.

--- shortestWordLength.js
+++ shortestWordLength.js
@@ -1,1 +1,1 @@
-const shortestWordLength = str => str.split(' ').sort((a, b) => a.length - b.length)[0].length;
+const shortestWordLength = str => str.replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"()*+,\-.\/:;<=>?\[\]_`{|}]/g,"").split(' ').sort((a, b) => a.length - b.length)[0].length;
